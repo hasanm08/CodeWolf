99927f0060f5b187904943071d312e7c88619fec
AmirHassan Amirmahani <hasanmahani08@gmail.com>
2021-02-12 23:10:25 +0330

    add animated menu

--- ProgrammingSchool/MainWindow.xaml.cs
+++ ProgrammingSchool/MainWindow.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using ProgrammingSchool.Windowses;
+using ProgrammingSchool.Windowses.Manager;
 using System.Windows;
+using System.Windows.Controls;
+
 namespace ProgrammingSchool
 {
     /// <summary>
@@ -11,23 +14,26 @@ namespace ProgrammingSchool
         {
             InitializeComponent();
         }
+        
 
         private void LoginClicked(object sender, RoutedEventArgs e)
         {
-            ContactUs contactUs = new ContactUs();
-            contactUs.Show();
-            AboutUs aboutUs = new AboutUs();
-            aboutUs.Show();
-            if (usernameTB.Text.Length > 0 && passwordTB.Password.Length > 0)
-            {
-                CustomMessageBox mbox = new CustomMessageBox($"نام کاربری:{usernameTB.Text}\nرمز عبور:{passwordTB.Password}");
-                mbox.Show();
-            }
-            else
-            {
-                CustomMessageBox mbox = new CustomMessageBox("خواهشا رمز عبور یا نام کاربری را وارد کنید.");
-                mbox.Show();
-            }
+            ManagerMainWindow managerMainWindow = new ManagerMainWindow();
+            managerMainWindow.Show();
+            //ContactUs contactUs = new ContactUs();
+            //contactUs.Show();
+            //AboutUs aboutUs = new AboutUs();
+            //aboutUs.Show();
+            //if (usernameTB.Text.Length > 0 && passwordTB.Password.Length > 0)
+            //{
+            //    CustomMessageBox mbox = new CustomMessageBox($"نام کاربری:{usernameTB.Text}\nرمز عبور:{passwordTB.Password}");
+            //    mbox.Show();
+            //}
+            //else
+            //{
+            //    CustomMessageBox mbox = new CustomMessageBox("خواهشا رمز عبور یا نام کاربری را وارد کنید.");
+            //    mbox.Show();
+            //}
         }
     }
 }
---
+++ ProgrammingSchool/Windowses/Manager/ManagerMainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ProgrammingSchool.Windowses.Manager
+{
+    /// <summary>
+    /// Interaction logic for ManagerMainWindow.xaml
+    /// </summary>
+    public partial class ManagerMainWindow : Window
+    {
+        public ManagerMainWindow()
+        {
+            InitializeComponent();
+        }
+        private void ButtonOpenMenu_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonCloseMenu.Visibility = Visibility.Visible;
+            ButtonOpenMenu.Visibility = Visibility.Collapsed;
+        }
+
+        private void ButtonCloseMenu_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonCloseMenu.Visibility = Visibility.Collapsed;
+            ButtonOpenMenu.Visibility = Visibility.Visible;
+        }
+
+        private void ListViewMenu_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            GridMain.Children.Clear();
+
+            UserControl usc;
+            //switch (((ListViewItem)((ListView)sender).SelectedItem).Name)
+            //{
+            //    case "ShowUsers":
+            //        usc = new UserControlHome();
+            //        GridMain.Children.Add(usc);
+            //        break;
+            //    case "AddUser":
+            //        usc = new UserControlCreate();
+            //        GridMain.Children.Add(usc);
+            //        break;
+            //    case "AddCourse":
+            //        usc = new UserControlCreate();
+            //        GridMain.Children.Add(usc);
+            //        break;
+            //    default:
+            //        break;
+            //}
+        }
+    }
+}
